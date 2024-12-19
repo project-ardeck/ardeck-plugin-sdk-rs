aa983a69594fe764b287725ba879afe7f201d7b8
akurakuu <77902040+akurakuu@users.noreply.github.com>
2024-12-19 16:21:12 +0900

    型

--- src/client.rs
+++ src/client.rs
@@ -1,57 +1,54 @@
 use serde::{Deserialize, Serialize};
+use serde_repr::{Deserialize_repr, Serialize_repr};
 
-pub mod websocket;
 pub mod env_args;
+pub mod websocket;
 
+// enum OPS {
+//     Hello
+// }
+
+#[derive(Serialize_repr, Deserialize_repr, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+#[repr(i32)]
 pub enum PluginOp {
     Hello,
     Success,
     Message,
-    Action
+    Action,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
-#[serde(rename_all = "camelCase")]
-pub struct PluginMessage {
-    // OP0: Hello
-    pub plugin_version: Option<String>,
-    pub ardeck_plugin_web_socket_version: Option<String>,
-    pub plugin_id: Option<String>,
-
-    // OP1: Success
-    pub ardeck_studio_version: Option<String>,
-    pub ardeck_studio_web_socket_version: Option<String>,
-
-    // OP2: Message
-    pub message_id: Option<String>,
-    pub message: Option<String>,
-
-    // OP3: Action
-    pub action_id: Option<String>,
-    pub action_data: Option<ActionMap>
-}
-
-impl Default for PluginMessage {
-    fn default() -> Self {
-        PluginMessage {
-            plugin_version: None,
-            ardeck_plugin_web_socket_version: None,
-            plugin_id: None,
-            ardeck_studio_version: None,
-            ardeck_studio_web_socket_version: None,
-            message_id: None,
-            message: None,
-            action_id: None,
-            action_data: None
-        }
-    }
+#[serde(rename_all = "camelCase", tag = "op", content = "data")]
+pub enum PluginMessageData {
+    Hello {
+        // OP0: Hello
+        plugin_version: String,
+        ardeck_plugin_web_socket_version: String,
+        plugin_id: String,
+    },
+    Success {
+        // OP1: Success
+        ardeck_studio_version: String,
+        ardeck_studio_web_socket_version: String,
+    },
+    Message {
+        // OP2: Message
+        message_id: String,
+        message: String,
+    },
+    Action {
+        // OP3: Action
+        action_id: String,
+        action_data: ActionMap,
+    },
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(rename_all = "camelCase")]
 pub enum SwitchType {
     Digital = 0,
-    Analog = 1
+    Analog = 1,
 }
 
 pub type SwitchId = u8;
@@ -62,7 +59,12 @@ pub struct ActionMap {
     pub switch_type: SwitchType,
     pub switch_id: SwitchId,
     pub plugin_id: String,
-    pub action_id: String
+    pub action_id: String,
 }
 
-
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct PluginMessage {
+    pub op: PluginOp,
+    pub data: PluginMessageData,
+}
--- src/client/websocket.rs
+++ src/client/websocket.rs
@@ -6,20 +6,26 @@ pub enum WebsocketState {
     Error,
 }
 
+type WebSocket = tokio_tungstenite::WebSocketStream<tokio_tungstenite::MaybeTlsStream<tokio::net::TcpStream>>;
+
 pub struct Websocket {
     state: WebsocketState,
+    stream: WebSocket,
 }
 
 impl Websocket {
     pub async fn new(port: &str) -> Self {
-        Self::connect(port).await;
+        let stream = Self::connect(port).await;
 
         Websocket {
             state: WebsocketState::None,
+            stream
         }
     }
 
-    async fn connect(port: &str,) {
-        
+    async fn connect(port: &str,) -> WebSocket {
+        let (ws, _) = tokio_tungstenite::connect_async(format!("ws://127.0.0.1:{port}")).await.unwrap();
+
+        ws
     }
 }
