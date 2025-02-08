12816380e070c7e5501b084680211d15260ae5c0
akurakuu <77902040+akurakuu@users.noreply.github.com>
2025-02-08 18:54:48 +0900

    batファイルの修正

--- plugin/build.bat
+++ plugin/build.bat
@@ -1,9 +1,9 @@
 @echo off
 
 cargo build -r
-
+cd %~dp0
 mkdir dist
-copy ..\target\release\ardeck-plugin-sdk-rs.exe .\dist\main.exe
+copy ..\target\release\ardeck-plugin.exe .\dist\main.exe
 copy .\manifest.json .\dist\manifest.json
 copy .\actions.json .\dist\actions.json
 
